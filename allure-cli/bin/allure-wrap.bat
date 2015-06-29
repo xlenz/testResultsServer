35d16d6dcd4e574452f8059113225b25da8b90ef
MG <xlenz7@gmail.com>
2015-06-30 00:32:04 +0300

    remove hardcoded allure version

--- allure-cli/bin/allure-wrap.bat
+++ allure-cli/bin/allure-wrap.bat
@@ -3,6 +3,6 @@ set outputPath=%2
 set allureVersion=%3
 rem set inputPath=allure-results_1435288828499
 rem set outputPath=site
-set allureVersion=1.4.14
+rem set allureVersion=1.4.14
 
 allure generate %inputPath% -o %outputPath% -v %allureVersion%
