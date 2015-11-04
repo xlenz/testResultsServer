3facc2c5584e4b65e4a5bb1e5422cfefc20f3b32
MG <xlenz7@gmail.com>
2015-11-04 13:46:48 +0200

    update allure

--- allure-cli/bin/allure-wrap.bat
+++ allure-cli/bin/allure-wrap.bat
@@ -1,6 +1,6 @@
 set inputPath=%1
 set outputPath=%2
-rem set inputPath=allure-results_1435395216501
+rem set inputPath=allure-results
 rem set outputPath=site
 
 allure generate %inputPath% -o %outputPath%
