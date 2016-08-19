72a348b11634f4c7ee2b63808c4689b8934e97c7
Mykola Grybyk <mgrybyk@serena.com>
2016-08-19 11:23:33 +0300

    Revert "jsconfig, eslint, upd gitignore"
    This reverts commit 3f389b900ee80a192e250c95574ba7902ae2f447.

--- allure-cli/bin/allure-wrap.bat
+++ allure-cli/bin/allure-wrap.bat
@@ -1,6 +1,6 @@
-rem set inputPath=%1
-rem set outputPath=%2
-set inputPath=allure-results
-set outputPath=allure-report
+set inputPath=%1
+set outputPath=%2
+rem set inputPath=allure-results
+rem set outputPath=allure-report
 
 allure generate %inputPath% -o %outputPath%
